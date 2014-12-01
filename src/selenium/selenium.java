ac52129a6fbac270915224f9bc073f2bcafd9c72
Donizete Debroski <donizetedebroski@outlook.com>
2014-12-01 20:01:55 -0200

    selenium comit

--- src/selenium/selenium.java
+++ src/selenium/selenium.java
@@ -4,8 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.Select;
 
 /**
  *
@@ -22,27 +21,42 @@ public class selenium {
 
         // Acessar o Site
         driver.get("http://www.minha.unidavi.edu.br/");
-
-        WebElement campoTexto = null;
         
-        //insere Usuário
-        campoTexto = driver.findElement(By.id("UWS_FRM_USR_NOME"));
-        campoTexto.sendKeys("31301");
-
-        //insere Senha
-        campoTexto = driver.findElement(By.name("UWS_FRM_USR_SENHA"));
-        campoTexto.sendKeys("******");
-
-        //Clica em Continuar
+        //Usuário
+        driver.findElement(By.id("UWS_FRM_USR_NOME")).clear();
+        driver.findElement(By.id("UWS_FRM_USR_NOME")).sendKeys("31301");
+        
+        //Senha
+        driver.findElement(By.name("UWS_FRM_USR_SENHA")).clear();
+        driver.findElement(By.name("UWS_FRM_USR_SENHA")).sendKeys("849328");
+        
+        //Continuar
         driver.findElement(By.name("acao")).click();
-       
-        //clica para ir para as materias
-        driver.findElement(By.className("brand")).click();
         
-        //clica para selecionar as materias
-        driver.findElement(By.className("dropdown-toggle")).click();
+        //Sala virtual
+        driver.findElement(By.linkText("Sala Virtual")).click();
         
-        driver.findElement(By.linkText("?pagina=EAD_ambiente_virtual_sala&sala=11797&passo=recados")).click();
-    }
+        //Acessar pagina Trabalhos
+        driver.findElement(By.linkText("Trabalhos")).click();
+        driver.findElement(By.xpath("//div[@id='svMenuTrabalhos']/ul/li[5]/a/span[2]")).click();
+        
+        //Clica em cadastrar novo Arquivo
+        driver.findElement(By.linkText("Cadastrar Novo Arquivo")).click();
+        driver.findElement(By.id("titulo")).click();
+        driver.findElement(By.id("titulo")).clear();
+        driver.findElement(By.id("titulo")).sendKeys("teste");
 
+        //Localiza Arquivo
+        driver.findElement(By.id("arquivo")).sendKeys("C:\\Users\\LENOVO\\Downloads\\selenium-ide-2.8.0.xpi");
+        driver.findElement(By.id("grupo")).click();
+        //Nova Pasta
+        new Select(driver.findElement(By.id("grupo"))).selectByVisibleText("Novo Pasta");
+        driver.findElement(By.cssSelector("option[value=\"novo\"]")).click();
+        driver.findElement(By.id("nomenovogrupo")).click();
+        driver.findElement(By.id("nomenovogrupo")).clear();
+        driver.findElement(By.id("nomenovogrupo")).sendKeys("teste2");
+        //Clica em cadastrar
+        driver.findElement(By.name("cadastrar")).click();
+    
+    }
 }
